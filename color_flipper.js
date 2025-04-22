23e23ed47eb5d74e23938ac658e86b69cec7c5fa
Adedmidun <154547213+Codelearner123456@users.noreply.github.com>
2025-04-22 18:48:32 +0100

    first commit

---
+++ color_flipper.js
@@ -0,0 +1,27 @@
+const colors = ['Red', 'Blue', 'Green', 'Yellow', 'Purple', 'Orange', 'Pink', 'Violet', 'Brown', 'Maroon', 'Gold', 'Navy', 'Crimson', 'Aqua', 'Turquoise', 'Silver', 'Teal', 'Wheat', 'Salmon', 'Lime'];
+const colorButton = document.getElementById('color-btn');
+const colorName = document.getElementById('color-name');
+
+function getRandomColor() {
+    const randomIndex = Math.floor(Math.random() * colors.length);
+    return colors[randomIndex];
+}
+
+colorButton.addEventListener('click', () => {
+    const newColor = getRandomColor();
+    document.body.style.colorName = newColor;
+    if (newColor === "Red" || newColor === "Blue" || newColor === "Green" || newColor === "Purple" || newColor === "Brown" || newColor === "Maroon" || newColor === "Navy" || newColor === "Crimson" || newColor === "Teal") {
+        colorName.classList.add('active');
+    } else {
+        colorName.classList.remove('active');
+    }
+    if (newColor === "Green") {
+        colorButton.classList.add('invert');
+    } ifelse (newColor === "Pink") {
+        colorButton.classList.a
+    }else {
+        colorButton.classList.remove('invert');
+    }
+    document.body.style.backgroundColor = newColor;
+    colorName.textContent = `Color: ${newColor}`;
+});
